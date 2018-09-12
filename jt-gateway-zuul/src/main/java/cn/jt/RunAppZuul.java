118ffebb52b80f50fe97ef49bca4c434271329b8
czy95czy <415299462@qq.com>
2018-09-12 14:13:41 +0800

    jtspringcloud

---
+++ jt-gateway-zuul/src/main/java/cn/jt/RunAppZuul.java
@@ -0,0 +1,16 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//@EnableCircuitBreaker //背后SpringCloud继承是Hystrix (@SpringCloudApplication已经继承)
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient  
+public class RunAppZuul {
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppZuul.class, args);
+	}
+}
